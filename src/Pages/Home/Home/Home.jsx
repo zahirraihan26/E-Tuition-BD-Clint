a1aab05823fed968d8c2e538f528d668e628d991
zahirraihan26 <mdzahirraihanbakul@gmail.com>
2026-05-11 23:33:05 +0600

    feat: implement main Home page component with section imports and layout structure

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -5,6 +5,7 @@ import Tuitions from '../../../Components/Tuitions/Tuitions';
 import TuitionSort from '../../../Components/Homepagedata/TuitionSort';
 import TuitorShort from '../../../Components/Homepagedata/TuitorShort';
 import WhyTutionHub from '../../../Components/Homepagedata/WhyTutionHub';
+import Testimonials from '../../../Components/Homepagedata/Testimonials';
 
 const Home = () => {
     return (
@@ -14,6 +15,7 @@ const Home = () => {
             <HowItWorks></HowItWorks>
             <TuitorShort></TuitorShort>
             <WhyTutionHub></WhyTutionHub>
+            <Testimonials></Testimonials>
         </div>
     );
 };
